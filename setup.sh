153c669da28208c8ff9e7e7d4bb5d49dac4e09af
wallaceeBenites <wallacepuck@gmail.com>
2025-05-25 17:07:41 -0300

    teste

--- setup.sh
+++ setup.sh
@@ -1,1 +1,14 @@
-sudo apt install -y iverilog
+#!/bin/bash
+
+# Compila e executa os testes
+iverilog -o simv Contador.v tb.v
+vvp simv
+
+# Verifica se a simulação foi bem sucedida
+if [ $? -eq 0 ]; then
+    echo "Todos os testes passaram!"
+    exit 0
+else
+    echo "Alguns testes falharam."
+    exit 1
+fi
